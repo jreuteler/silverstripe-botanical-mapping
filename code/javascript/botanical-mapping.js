3f56fdc8818e39b80439d99af5f61881fdf1a30c
Jan Reuteler <janreuteler@icloud.com>
2017-04-19 09:43:52 -0500

    proof of concept of autocomplete-redirect

--- code/javascript/botanical-mapping.js
+++ code/javascript/botanical-mapping.js
@@ -10,3 +10,45 @@
         });
     });
 })(jQuery);
+
+
+
+
+// adds change listener to value holder field and redirects to the elements edit-page (proof in concept)
+(function ($) {
+    $.entwine('ss.autocomplete-edit', function ($) {
+
+        $('.field.autocomplete.autocomplete-edit input.text').entwine({
+
+            onmatch: function () {
+
+                var input = $(this);
+                var hiddenInput = input.parent().find(':hidden');
+                var valueLabel = input.parent().find('.value-holder .value');
+
+                valueLabel.bind("DOMSubtreeModified", function () {
+                    var segmentStr = window.location.pathname;
+                    var segmentArray = segmentStr.split('/');
+                    var objectType = input.attr('name');
+                    var objectTypeArray = objectType.split('|');
+                    var redirectUrl = '';
+                    for (i = 0; i < segmentArray.length; i++) {
+                        redirectUrl += segmentArray[i] + '/';
+
+                        if (segmentArray[i] == 'botanical-frontend') {
+                            break;
+                        }
+                    }
+
+                    redirectUrl += objectTypeArray[0] + '/edit/' + hiddenInput.val();
+                    window.location = redirectUrl;
+
+                });
+
+
+            },
+
+
+        });
+    });
+})(jQuery);
